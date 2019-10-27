58c825c76141a9356ce5d02c3373c63a503f2e1c
gagikjs <gagik@princeton.edu>
2019-10-27 00:58:07 -0400

    Added manifest and saving.

--- dashboard/save.js
+++ dashboard/save.js
@@ -1,3 +1,6 @@
+var currentDocId = 0;
+const USER_DATA = "USER_DATA";
+
 function err() {
     M.toast({html: 'Please enter a valid value'});
 }
@@ -33,11 +36,21 @@ function getGoal(goalNumber) {
     return newObj;
 }
 
+function modifyDocData(docId, options) {
+    chrome.storage.local.get(USER_DATA, (data) => {
+        data = data[USER_DATA];
+        data[docId] = !data[docId] ? {} : data[docId];  
+        data[docId].options = options;
+        chrome.storage.local.set({USER_DATA: options});
+    });
+}
+
 $(document).ready(function() {
     $("#begin").click(function() {
         let arr = [];
         let p1 = getGoal(1);
         let p2 = getGoal(2);
         if(p1 == {}) return;
+        modifyDocData(currentDocId, {...p1, ...p2});
     });
 });
