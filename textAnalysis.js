d28bd3de5a3c6e9b5171c557bc907b6147534890
Warren James <james@notraffic.tech>
2019-10-27 03:21:09 -0400

    Update content scripts

--- textAnalysis.js
+++ textAnalysis.js
@@ -1,50 +1,59 @@
 // creates a button object to be clicked on by someone to start the chrome extension
+const DOC_REGEX = /https:\/\/docs\.google\.com\/document\/d\/(.*)\/edit/;
+const NEWLINE = 10;
+const SPACE1 = 160;
+const SPACE2 = 8204;
+const TABSPACE = 9;
+
+let updater;
 function startButton() {
     let button = document.createElement("button");
+    button.textContent = "hi";
     button.addEventListener("click", function() {
-        console.log();
+        console.log("Hello there");
     });
 
     let target=document.getElementsByClassName("docs-title-outer docs-title-inline-rename");
     target[0].appendChild(button);
 }
-
 startButton();
 
-// FUNCTION NEEDS TO BE ADDRESSED, HOW TO DETERMINE WHEN TO STOP.
+// TODO: FUNCTION NEEDS TO BE ADDRESSED, HOW TO DETERMINE WHEN TO STOP.
 function runUpdates() {
     let checkOn;
     
-    chrome.storage.local.get(USER_DATA, (data) => {
-        data = data[USERDATA];
+    chrome.storage.local.get("USER_DATA", (data) => {
+        data = data.USER_DATA;
         checkOn = data.isOn;
     });
 
-        setInterval(()=>{
-            update();
-            chrome.storage.local.get(USER_DATA, (data) => {
-                data = data[USERDATA];
-                checkOn = data.isOn;
-            });
-        }, 200);
+    updater = setInterval(()=>{
+        update();
+        chrome.storage.local.get("USER_DATA", (data) => {
+            data = data.USER_DATA;
+            checkOn = data.isOn;
+        });
+    }, 200);
 }
 
 // Counts paragraphs and words on the document and updates values in storage.local if
 // any values have changed.
 function update() {
+    let docID = window.location.href.match(DOC_REGEX)[1];
     let pCount = countParagraphs();
     let wCount = countWords();
 
-    const addValues = (pCount, wCount) => {chrome.storage.local.get(USER_DATA, (data) => {
-            data = data[USERDATA];
+    const addValues = (pCount, wCount) => {
+        chrome.storage.local.get("USER_DATA", (data) => {
+            data = data.USER_DATA;
             
-            if (data.paragraphCount !== pCount) {
-                data.paragraphCount = pCount;
+            if (data[docID].paragraphCount !== pCount) {
+                data[docID].paragraphCount = pCount;
                 chrome.storage.local.set({USER_DATA : data});
             }
 
-            if (data.wordCount !== wCount) {
-                data.wordCount = wCount;
+            if (data[docID].wordCount !== wCount) {
+                data[docID].wordCount = wCount;
                 chrome.storage.local.set({USER_DATA: data});
             }
         });
@@ -60,8 +69,9 @@ function countWords() {
         alert("Element does not exist on the document.")
     }
     const pageText = page.innerText;
-    const words = pageText.split(" ");
-    
+    let words = pageText.split(/\s+/);
+    words = words.filter((word) => (!((word.charCodeAt(0) == 8204) && (word.length == 1))));
+    console.log(words[6].length, words[7].length);
     return words.length; 
 }
 
@@ -85,10 +95,7 @@ function countLines() {
 // Intended for Google Docs. Counts and returns the number of paragraphs on the current document. 
 // Paragraphs are defined as two consecutive new line characters after character text.
 function countParagraphs() {
-    const NEWLINE = 10;
-    const SPACE1 = 160;
-    const SPACE2 = 8204;
-    const TABSPACE = 9;
+    
 
     let page = document.getElementsByClassName("kix-zoomdocumentplugin-outer")[0];
     if (page == null) {
@@ -127,3 +134,5 @@ function countParagraphs() {
     
     return paragraphCount;
 }
+
+runUpdates();
